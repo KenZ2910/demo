67295a8dcd17f65a749775fba9c8232726c90825
KenZ2910 <khaivdps40416@gmail.com>
2024-10-29 16:58:31 +0700

    web502

---
+++ buoi1.1.js
@@ -0,0 +1,7 @@
+var arra=["Nguyen Van A","Nguyen Van B","Nguyen Van C"];
+function printName(arr){
+    for(let i=0; i<arr.length;i++){
+        console.log(`Ten sinh vien: ${arr[i]}`);
+    }
+}
+printName(arra);
---
+++ buoi1.js
@@ -0,0 +1,7 @@
+let name = "ABC"
+console.log(`Toi ten la: ${name}`);
+function count(count){
+    for(let i=0 ; i<count;i++)
+        console.log(`So: ${i}`);
+}
+count(10);
